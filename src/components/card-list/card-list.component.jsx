55fd0fb4bd00d737b9104ae18e72c574a5211b33
Shubhamsharma242 <shubhamsharma242242@gmail.com>
2023-08-15 00:41:13 +0530

    add vscode settings

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,8 @@
-import Card from '../card/card.component';
-import './card-list.style.css';
+import Card from "../card/card.component";
+import "./card-list.style.css";
 
 const CardList = ({ monsters }) => (
-  <div className='card-list'>
+  <div className="card-list">
     {monsters.map((monster) => {
       return <Card key={monster.id} monster={monster} />;
     })}
